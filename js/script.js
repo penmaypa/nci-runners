4fe099aad9a8cfcf48d52fe4879b49437e7a9420
Penuel Maypa <penmayp@gmail.com>
2017-08-30 22:07:04 +0100

    added timer on the notice

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+function onload() {
+  setTimeout(
+      function() {
+        document.getElementById("my-notice").style.display='block';
+      }, 2000);
+}
+
+
+
+
 function close_not(){
   document.getElementById("my-notice").style.display = "none";
 
